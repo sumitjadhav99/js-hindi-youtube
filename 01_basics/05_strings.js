4353b0a1f18d3d5343d4dd013f33893095b6753f
Sumit Jadhav <79534431+sumitjadhav99@users.noreply.github.com>
2024-10-31 04:55:05 +0000

    strings in js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "Sumit"
+const repoCount = "20"
+
+// console.log(name + repoCount + " value")
+
+// Modern way
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)
+
+const gameChange = new String('Sumithjadhav')
+// console.log(gameChange[0])
+// console.log(gameChange.__proto__)
+// console.log(gameChange.length)
+// console.log(gameChange.toUpperCase())
+// console.log(gameChange.charAt(2));
+// console.log(gameChange.indexOf('t'));
+// console.log(gameChange.substring(1, 4))
+// console.log(gameChange.slice(1,-2));
+
+const newStringOne = "         extraSpaces     "
+// console.log(newStringOne);
+
+// console.log(newStringOne.trim())
+
+const url = 'https://hitesh.com/hitesh%20choudhary'
+// console.log(url.replace('20', '-'))
+// console.log(url.includes('sundar'))
+
+const newStringTwo = "sumit is back in game"
+console.log(newStringTwo.split(' ', 2))
+console.log(newStringTwo.split(' '))
+
+
+
