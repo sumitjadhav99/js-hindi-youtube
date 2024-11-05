2d9a66c3aefb15caa1ad766ab859228c0fd56a3c
Sumit Jadhav <79534431+sumitjadhav99@users.noreply.github.com>
2024-11-05 02:22:39 +0000

    Math and nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+//Numbers
+
+const score = 400
+
+const balance = new Number(101.8999)
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed())
+// console.log(balance.toPrecision(3))
+
+const hundreads = 1000000
+// console.log(hundreads.toLocaleString('en-IN'))
+
+/*******Maths******/
+// console.log(Math);
+// console.log(Math.abs(-32.9))
+// console.log(Math.round(32.78));
+// console.log(Math.ceil(32.1))
+// console.log(Math.floor(33.99))
+// console.log(Math.min(1,2,3,4,5))
+// console.log(Math.max(1,2,3,4,5))
+
+// console.log(Math.random()*10+1)
+// console.log(Math.floor(Math.random()*10)+1))
+
+const min = 10 
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ 10 )
+
