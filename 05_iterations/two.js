81a41b0cbbe921bfb6c5a420b6bce718e98045f6
Sumit Jadhav <79534431+sumitjadhav99@users.noreply.github.com>
2024-11-23 16:03:23 +0000

    while and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// while loop
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of index is: ${index}`)
+//     index += 2;
+// }
+
+// While loop to access array values
+let myArray = ['reactJs', 'typeScript', 'angular']
+
+let arr = 0
+
+while (arr < myArray.length) {
+    // console.log(`Variants of js are: ${myArray[arr]}`)
+    arr++;
+}
+
+// do while loop
+let score = 11
+
+do {
+    console.log(score)
+    score++
+} while (score <=10);
