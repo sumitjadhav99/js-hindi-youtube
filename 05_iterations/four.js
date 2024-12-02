50e3f92db9e0756d65ff4e8c4319121c80973c14
Sumit Jadhav <79534431+sumitjadhav99@users.noreply.github.com>
2024-12-02 14:33:22 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,18 @@
+// for in loop
+
+const myObject = {
+    cpp: 'C++',
+    js: "JavaScript",
+    rb: "Ruby",
+    swift: "Swift by Apple"
+}
+
+for(key in myObject){
+    // console.log(`${key} shortcut is for ${myObject[key]}`)
+}
+
+const progrraming = ['cpp', 'python', 'java', 'js', 'ruby']
+
+for(key in progrraming){
+    console.log(progrraming[key])
+}
